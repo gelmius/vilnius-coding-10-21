599600b1077f1ddb6fe6fe9b89fc2e32b2871f75
selmi <selmi@ORCHIDEJA>
2024-10-22 09:15:12 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+//4 UZDUOTIS
+//Console.WriteLine("iveskite pagrinda ir auksti");
+
+//float plotas = 0, pagrindas = 0, aukstis = 0;
+//plotas = pagrindas * aukstis / 2;
+//Console.WriteLine("plotas = " + plotas);
+
+//5 UZDUOTIS
+
+//Console.WriteLine("iveskite du skaicius");
+//float vienasskaicius = float.Parse(Console.ReadLine());
+//float antrasskaicius = float.Parse(Console.ReadLine());
+//float vienasskaicius = float.Parse(Console.ReadLine());
+
+//plotas = pagrindas * aukstis / 2;
+
+//Console.WriteLine("suma = " + (vienasskaicius + antrasskaicius));
+
+//Console.WriteLine("skirtumas = " + (vienasskaicius - antrasskaicius));
+
+//Console.WriteLine("sandauga = " + (vienasskaicius * antrasskaicius));
+
+//if(antrasskaicius != 0)
+//{
+//    Console.WriteLine("dalyba = " + (vienasskaicius / antrasskaicius));
+//}
+
+//Console.WriteLine("kvadratas = " + (vienasskaicius * vienasskaicius));
+
+//Console.WriteLine("kvadratas = " + (antrasskaicius * antrasskaicius));
+
+//Console.WriteLine("vidurkis = " + ((vienasskaicius + antrasskaicius + treciasskaicius) / 3 ));
+
+// 6 UZDUOTIS
+
+//Console.WriteLine("iveskite temperatura celsijaus laipsniais");
+//float celsijai = float.Parse(Console.ReadLine());
+
+//Console.WriteLine(celsijai * (9 / 5) + 32);
+//Console.WriteLine();
+
+//7 UZDUOTIS
+
+//Console.WriteLine(" įveskite savo svorį kilogramais ir ūgį metrais");
+//float kilogramai = 0, ugis=0;
+
+//kilogramai = float.Parse( Console.ReadLine());
+//ugis = float.Parse(Console.ReadLine());
+
+//Console.WriteLine("KMI = " + kilogramai / (ugis*ugis));
+
+//8 UZDUOTIS
+
+//Console.WriteLine(" iveskite darbo užmokestį ir dirbtų valandų skaičių per savaitę");
+
+//float valandinis = float.Parse(Console.ReadLine());
+//float valandos = float.Parse(Console.ReadLine());
+
+//Console.WriteLine("Savaitinis uzmokestis = " + valandinis * valandos);
+//Console.WriteLine("Menesinis uzmokestis = " + valandos * valandinis * 4);
+//Console.WriteLine("Metinis uzmokestis = " + valandos * valandinis * 4);
+
+//Užduotis: Apskritimo ploto skaičiavimas
+
+//Console.WriteLine("įveskite apskritimo spindulį");
+//float spindulys;
+//spindulys = float.Parse( Console.ReadLine());
+//Console.WriteLine(spindulys * spindulys * float.Pi);
+
+//Užduotis: Procentų skaičiavimas
+
+//Console.WriteLine(" įveskite skaičių ir procentą");
+
+//float skaicius, procentas;
+//skaicius = float.Parse( Console.ReadLine());
+//procentas = float.Parse(Console.ReadLine());
+
+//Console.WriteLine(skaicius/100*procentas);
+
+//Užduotis: Valiutų konvertavimas(Litai, eurai, doleriai, svarai)
+
+//Console.WriteLine("Iveskite suma litais");
+//double suma, eur, dol;
+//suma = double.Parse( Console.ReadLine());
+//eur = suma / 3.4528;
+//dol = eur * 0.93;
+
+//Console.WriteLine(eur + "eur, " + dol + "dol, " + dol / 1.31 + "svaru");
+
+Console.ReadLine();
